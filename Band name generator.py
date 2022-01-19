33d7136ff064121c884a7f40ee4d098be262040b
Muhammad Tanbal <Muhammad.barcelona1@gmail.com>
2022-01-19 14:34:18 -0500

    Create Band name generator.py

---
+++ Band name generator.py
@@ -0,0 +1,8 @@
+#welcome message
+print ("Welcome to band name Generator")
+#get the city name
+city_name = input("what's the name of your city?\n")
+#get the pet name
+pet_name = input("what's your pet's name?\n")
+#funny band name
+print ("your band name could be " + city_name + " " + pet_name )
